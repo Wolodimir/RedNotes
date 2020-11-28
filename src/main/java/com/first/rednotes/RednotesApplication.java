7dc2b639d7159c9f56ffd2783a2b48f42978d68f
Wolodimir <dvdnent666@gmail.com>
2020-11-28 22:42:49 +0400

    First initialisation.

---
+++ src/main/java/com/first/rednotes/RednotesApplication.java
@@ -0,0 +1,13 @@
+package com.first.rednotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RednotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RednotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/first/rednotes/RednotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.rednotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RednotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
